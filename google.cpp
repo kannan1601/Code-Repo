b25f6a266b9f6f219a19a4f88ddc364e1b9e59b2
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-07-25 11:07:50 +0530

    Add files via upload

---
+++ google.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve()
+{
+	
+ 
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	for(int i=1;i<=t;i++)
+	{
+		cout<<"case #"<<i<<":"<<" ";
+		solve();
+	}
+}
