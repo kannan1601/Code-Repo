a23792fbfe5989c5529d822b342918701a263c3f
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-07-26 19:19:52 +0530

    Update 1,linked_list_implentation.cpp

--- 1,linked_list_implentation.cpp
+++ 1,linked_list_implentation.cpp
@@ -28,17 +28,7 @@ int main()
 			temp=newnode;
 		}
 	}
-//	int n2;
-//	cout<<"press 1 if you want to insert a node at beginning ";
-//	cin>>n2;
-//	if(n2==1)
-//	{
-//		newnode=(struct node*)malloc(sizeof(struct node));
-//		cout<<"enter data you want insert ";
-//		cin>>newnode->data;
-//		newnode->next=head;
-//		head=newnode;
-//	}
+
 	temp=head;
 	for(int i=1;i<=n+1;i++)
 	{
