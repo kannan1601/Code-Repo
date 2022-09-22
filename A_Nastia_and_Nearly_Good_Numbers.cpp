8a6a942c166f89e7d09c6b0594af1e8f83a57ce8
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-09-22 07:42:54 +0530

    Add files via upload

---
+++ A_Nastia_and_Nearly_Good_Numbers.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int gcd(int a,int b)
+{ if(b==0) return a; else{ return gcd(b,a%b); } }
+bool isPrime(ll n)
+{ if(n==1) return false; else { for(ll i = 2;i*i<=n;i++) { if(n%i==0)return false; } return true; } }
+void solve()
+{	
+	ll A, B;
+	cin>>A>>B;
+	if(B == 1)
+	{
+		cout<<"NO\n";
+
+	}
+	else
+	{
+		cout<<"YES\n";
+		cout<<A * B<<" "<<A<<" "<<A * B + A<<"\n";
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
