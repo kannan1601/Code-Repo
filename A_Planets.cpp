650e1f68fe71ee3bf2e6087bd4197c70b0b93654
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-09-25 21:29:52 +0530

    Add files via upload

---
+++ A_Planets.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int gcd(int a,int b)
+{ if(b==0) return a; else{ return gcd(b,a%b); } }
+bool isPrime(ll n)
+{ if(n==1) return false; else { for(ll i = 2;i*i<=n;i++) { if(n%i==0)return false; } return true; } }
+void solve()
+{	
+	ll n, c;
+  cin >> n >> c;
+  ll a[n];
+  map<int,int> mpp;
+  for(int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+    mpp[a[i]]++;
+  }
+  ll cost = 0;
+  for(auto it : mpp)
+  {
+    if(it.second > 1)
+    {
+      if(it.second < c)
+      {
+        cost+=it.second;
+      }
+      else
+      cost+=c;
+    }
+    else
+    cost++;
+  }
+  cout<<cost<<"\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
