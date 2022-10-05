4a7e8082e14d660c02ef94bf6464185d8563faa5
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-10-05 17:42:26 +0530

    Add files via upload

---
+++ B_Permutation_Sort.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+const ll INF = 2e18;
+int gcd(int a,int b)
+{ if(b==0) return a; else{ return gcd(b,a%b); } }
+bool isPrime(ll n)
+{ if(n==1) return false; else { for(ll i = 2;i*i<=n;i++) { if(n%i==0)return false; } return true; } }
+
+void solve()
+{	
+	int n;
+  cin >> n;
+  int a[n + 1];
+  int curr = 0,flag = 0;
+  for(int i = 1; i <= n; i++)
+  {
+    cin >> a[i];
+    if(curr < a[i])
+    {
+      curr = a[i];
+    }
+    else
+    {
+      flag = 1;
+    }
+  }
+  if(!flag)
+  {
+    cout << "0\n";
+    return;
+  }
+  if(a[1] == 1 || a[n] == n)
+  {
+    cout << "1\n";
+    return;
+  }
+  if(a[1] == n && a[n] == 1)
+  {
+    cout << "3\n";
+    return;
+  }
+  cout << "2\n";
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
