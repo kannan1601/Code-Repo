e446e20b3dcbf0d56a1116e2f8d2106b32389e7d
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-11-06 11:11:29 +0530

    Add files via upload

---
+++ B_Combinatorics_Homework.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+
+void solve()
+{	
+	int a, b, c, m;
+  cin >> a >> b >> c >> m;
+  int hi = a + b + c - 3;
+  vector<int> x = {a,b,c};
+  sort(x.begin(), x.end());
+  int lo = x[2] - 1 - x[0] - x[1];
+  if(m >= lo && m <= hi){
+    cout << "YES\n";
+  }
+  else 
+    cout << "NO\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
