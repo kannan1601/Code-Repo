4248575676fe5fc6b0e34606b00c2aaa8c660e07
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-11-11 22:21:44 +0530

    Add files via upload

---
+++ B_Sifid_and_Strange_Subsequences.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+
+void solve()
+{	
+	int n;
+  cin >> n;
+  vector<int> a;
+  int x;
+  int plus = INT_MAX;
+  int cnt_plus = 0;
+  for(int i = 0; i < n; i++){
+    cin >> x;
+    if(x <= 0)
+      a.pb(x);
+    else{
+      plus = min(plus, x);
+      cnt_plus = 1;
+    }
+  }
+  
+  sort(a.begin(), a.end());
+  int mini = INT_MAX;
+  for(int i = 1; i < a.size(); i++){
+    int diff = abs(a[i] - a[i - 1]);
+    mini = min(mini, diff);
+  }
+  // cout << mini;
+  if(mini >= plus){
+    cout << a.size() + cnt_plus <<"\n";
+  }
+  else{
+    cout << a.size() <<"\n";
+  }
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
