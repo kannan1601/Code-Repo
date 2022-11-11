403f543098e8596f7f8a5ccbe12327595827593e
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-11-11 11:17:26 +0530

    Add files via upload

---
+++ B_Moamen_and_k-subarrays.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+
+void solve()
+{	
+	ll n, k;
+  cin >> n >> k;
+  vector<pair<int,int>> a(n);
+  for(int i = 0; i < n; i++){
+    cin >> a[i].first;
+    a[i].second = {i};
+  }  
+  int cnt = 1;
+  sort(a.begin(), a.end());
+  for(int i = 1; i < n; i++){
+    if(a[i - 1].second + 1 != a[i].second){
+      cnt++;
+    }
+  }
+  cout << (cnt <= k ? "YES\n" : "NO\n"); 
+
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
