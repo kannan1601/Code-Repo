eb66bf795c1decc9d1df2b26ece8b66d8a45ae33
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-11-19 22:16:06 +0530

    Add files via upload

---
+++ A_Yes-Yes.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+
+bool check(int i, string s, string tmp){
+  for(int j = 0; j < s.length(); j++){
+    if(s[j] != tmp[(i + j) % 3])
+      return false;
+  }
+  return true;
+}
+
+void solve()
+{	
+  string s;
+  cin >> s;
+  string tmp = "Yes";
+  if(check(0, s, tmp) || check(1, s, tmp) || check(2, s, tmp)){
+    cout << "YES\n";
+  }
+  else{
+    cout << "NO\n";
+  }
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
