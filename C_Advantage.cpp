34aba54ec5823716ecd6c26a01e40e723012e9b4
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-11-21 21:00:53 +0530

    Add files via upload

---
+++ C_Advantage.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+
+void solve()
+{
+	ll n;
+  cin >> n;
+  vector<ll> a(n);
+  for(ll i = 0; i < n; i++){
+    cin >> a[i];
+  }
+  vector<ll> b = a;
+  sort(b.begin(), b.end());
+  ll mx = b[n - 1];
+  ll scndmx = b[n - 2];
+
+  for(int i = 0; i < n; i++){
+    if(a[i] == mx){
+      cout << a[i] - scndmx <<" ";
+    }
+    else{
+      cout << a[i] - mx <<" ";
+    }
+  }
+  cout << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
