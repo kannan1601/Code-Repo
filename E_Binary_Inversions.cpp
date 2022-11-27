e17dc6f4013d15091ae1ffe1267eb98868672d7b
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-11-27 09:43:10 +0530

    Add files via upload

---
+++ E_Binary_Inversions.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+
+ll cal(vector<int> &a){
+  ll ones = 0;
+  ll val = 0;
+  for(ll i = 0; i < a.size(); i++){
+    if(a[i] == 1)ones++;
+    else{
+      val+=ones;
+    }
+  }
+  return val;
+}
+void solve()
+{
+  ll n;
+  cin >> n;
+  vector<int> a(n);
+  for(ll i = 0; i < n; i++){
+    cin >> a[i];
+  }
+  ll ans = cal(a);
+  for(ll i = 0; i < n; i++){
+    if(a[i] == 0){
+      a[i] = 1;
+      ans = max(ans, cal(a));
+      a[i] = 0;
+      break;
+    }
+  }
+  for(ll i = n - 1; i >= 0; i--){
+    if(a[i] == 1){
+      a[i] = 0;
+      ans = max(ans, cal(a));
+      a[i] = 1;
+      break;
+    }
+  }
+  cout << ans <<"\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
