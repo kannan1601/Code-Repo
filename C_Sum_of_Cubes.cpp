d961ddaebcafc2ac7e595b46b5d687d02e0e3da8
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-11-28 20:33:29 +0530

    Add files via upload

---
+++ C_Sum_of_Cubes.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+const ll N = 1000000000001;
+set<ll> s;
+void precalc(){
+  for(ll i = 1; i * i * i <= N; i++){
+    s.insert(i * i * i);
+  }
+}
+
+void solve()
+{
+  ll X;
+  cin >> X;
+  for(ll i = 1; i * i * i <= X; i++){
+    if(s.count(X - i * i * i)){
+      cout << "YES\n";
+      return;
+    }
+  }
+  cout << "NO\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+  precalc();
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
