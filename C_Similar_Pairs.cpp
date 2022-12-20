f2505758d03e8c023ca98b78d4e081eb8c819101
Kannan <72595590+kannan1601@users.noreply.github.com>
2022-12-20 19:41:39 +0530

    Add files via upload

---
+++ C_Similar_Pairs.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+
+void solve()
+{
+  int N;
+  cin >> N;
+  vector<int> odd, even;
+  for(int i = 0; i < N; i++){
+    int x;
+    cin >> x;
+    if(x % 2)
+      odd.pb(x);
+    else
+      even.pb(x);
+  }
+  if(odd.size() % 2 == 0 && even.size() % 2 == 0){
+    cout << "YES\n";
+    return;
+  }
+  for(int i = 0; i < even.size(); i++){
+    for(int j = 0; j < odd.size(); j++){
+      if(abs(even[i] - odd[j]) == 1){
+        cout << "YES\n";
+        return;
+      }
+    }
+  }
+  cout << "NO\n";
+  
+  
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
