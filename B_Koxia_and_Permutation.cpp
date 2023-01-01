af6116888e1e457f215001a600adfb51c8d037f2
Kannan <72595590+kannan1601@users.noreply.github.com>
2023-01-01 10:06:24 +0530

    Add files via upload

---
+++ B_Koxia_and_Permutation.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+
+const ll MOD = 1000000007;
+
+void solve()
+{
+	int N, K;
+  cin >> N >> K;
+  int n = N, j = 1;
+  for(int i = 2; i <= N; i+=2){
+    cout << n <<" ";
+    n--;
+    cout << j <<" ";
+    j++;
+  }
+  if(N % 2){
+    cout << N/2 + 1 <<"\n"; 
+  }
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
