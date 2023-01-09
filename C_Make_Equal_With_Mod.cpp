82bd8f439c7b5ad664c86940f20a67e0ff232f1e
Kannan <72595590+kannan1601@users.noreply.github.com>
2023-01-09 22:12:19 +0530

    Add files via upload

---
+++ C_Make_Equal_With_Mod.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+const ll MOD = 1000000007;
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+ll inv(ll i) {if (i == 1) return 1; return (MOD - ((MOD / i) * inv(MOD % i)) % MOD) % MOD;}
+
+
+void solve()
+{
+	int N;
+  cin >> N;
+  vector<int> A(N);
+  for(int i = 0; i < N; i++){
+    cin >> A[i];
+  }
+  bool one = false, consec = false;
+  sort(A.begin(), A.end());
+  for(int i = 0; i < N; i++){
+    if(A[i] == 1)
+      one = true;
+    if(i < N - 1){
+      if(A[i] + 1 == A[i + 1])
+        consec = true;
+    }
+  }
+  cout << ((one && consec) ? "NO\n" : "YES\n"); 
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
