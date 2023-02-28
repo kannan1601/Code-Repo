7bfbc284b66631b7a91cae0b55ca8ea78dd7f275
Kannan <72595590+kannan1601@users.noreply.github.com>
2023-02-28 19:12:58 +0530

    Add files via upload

---
+++ B_AccurateLee.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long double
+#define pb push_back
+#define all(v) (v).begin(),(v).end()
+typedef vector<int> vi;
+typedef vector<ll> vll;
+const ll MOD = 1000000007;	
+
+bool isPrime(ll n)
+{if(n==1) return false; else {for(ll i = 2;i*i<=n;i++) {if(n%i==0)return false;} return true;}}
+vector<ll> sieve(int n) {int arr[n];for(int i = 0; i < n; i++)arr[i] = 0; vector<ll> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = i * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPowerofTwo(ll n){if(n == 0) return false; else return (ceil(log2(n)) == floor(log2(n)));}
+ll Powermod(ll base, ll power, ll x){ll res = 1;while(power){if(power % 2 == 0){base = (base * base) % x;power/=2;}else{res = (res * base) % x; power--;}}return res;}
+ll inv(ll i) {if (i == 1) return 1; return (MOD - ((MOD / i) * inv(MOD % i)) % MOD) % MOD;}
+
+void solve()
+{
+	int N;
+  cin >> N;
+  string S;
+  cin >> S;
+  bool hasZ = false, hasO = false;
+  for(int i = 0; i < N; i++){
+    if(S[i] == '0')
+      hasZ = true;
+    else
+      hasO = true;
+  }
+  if(!hasZ || !hasO){
+    cout << S << "\n";
+    return;
+  }
+  int one = -1, zero = -1;
+  for(int i = 0; i < N; i++){
+    if(S[i] == '1'){
+      one = i;
+      break;
+    }
+  }
+  for(int i = N - 1; i >= 0; i--){
+    if(S[i] == '0'){
+      zero = i;
+      break;
+    }
+  }
+  if(one > zero){
+    cout << S << "\n";
+    return;
+  }
+  string ans = "";
+  for(int i = 0; i < one; i++){
+    ans += S[i];
+  }
+  ans += '0';
+  zero++;
+  for(int i = zero; i < N; i++){
+    ans += S[i];
+  }
+  cout << ans << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
+
+	
